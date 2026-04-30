10ed174729cfb8ba2b90b186e7e18f572519bac6
uniitee <ghildiyaluma0@gmail.com>
2026-04-30 19:44:57 +0530

    Refactor code structure for improved readability and maintainability

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,13 +5,16 @@ import FocusCardsDemo from "@/components/focus-cards-demo";
 export default function Projects() {
     return (
       <div className="overflow-hidden sm:px-10 px-5 w-full min-h-svh">
-        <div className="mt-32 sm:mt-44 w-full flex flex-col items-center justify-center gap-4">
+        <div className="mt-32 sm:mt-40 w-full flex flex-col items-center justify-center gap-4">
           <div className="flex flex-col text-center font-sacramento mb-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl text-blue-300">
               PROJECTS
             </h1>
-            <p className="text-md md:text-lg text-zinc-400 text-justify w-full max-w-4xl mt-2">
-              Of course, I have some projects that I have worked on, I mean that was the whole point of this portfolio. Some were made solely to fill the void -ahm of this  website and my heart- but trust me on this one, its not like they don't solve problems.
+            <p className="text-md md:text-lg text-zinc-400 text-center w-full max-w-4xl mt-2">
+              And miles to go before I sleep,
+            </p>
+            <p className="text-md md:text-lg text-zinc-400 text-center w-full max-w-4xl ">
+              And miles to go before I sleep.
             </p>
           </div>
           <FocusCardsDemo />
